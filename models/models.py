c223305e2aac4cfde62e81b7338e061a3d58b67c
Eduardo Vizcaino <evigra@gmail.com>
2022-01-05 21:04:36 -0600

    UPDATE

--- models/models.py
+++ models/models.py
@@ -74,7 +74,7 @@ class tc_devices(models.Model):
     signal                                      = fields.Boolean('Good signal', default=True)
     company_ids                                 = fields.Many2many('res.company', 'tcdevices_res_company_rel', 'user_id', 'cid', string='Companies', default=lambda self: self.env.user.company_id)
     
-    
+    """
 class tc_positions(models.Model):
     _name = "tc_positions"
     _description = 'traccar positions'
@@ -94,7 +94,7 @@ class tc_positions(models.Model):
     address                                     = fields.Char('Calle', size=150)
     attributes                                  = fields.Char('Atributos', size=5000)
     status                                      = fields.Char('Type', size=5000)
-
+    """
     
 class vehicle(models.Model):
     _inherit = "fleet.vehicle"
